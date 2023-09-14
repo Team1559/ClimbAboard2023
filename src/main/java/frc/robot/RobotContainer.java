a7d1715fffb36aa579dde6d225daeb09c89e72c1
Software <1559DevilTech@gmail.com>
2023-09-13 20:26:14 -0400

    added shooter subsystem and commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,54 +4,66 @@
 
 package frc.robot;
 
-import frc.lib.DTXboxController;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.DriveCommand;
-import frc.robot.subsystems.DriveTrain;
-
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.lib.DTXboxController;
+import frc.robot.commands.DriveCommand;
+import frc.robot.commands.FeederCommand;
+import frc.robot.commands.ShooterCommand;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.FrisbeeFlinger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+    // The robot's subsystems and commands are defined here...
 
-  private DriveCommand driveCommand;
+    private DriveTrain driveTrain;
 
-  private DriveTrain driveTrain;
+    private FrisbeeFlinger frisbeeFlinger;
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private final DTXboxController controller = new DTXboxController(0);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final DTXboxController controller =
-      new DTXboxController(0);
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure the shooter and feeder
+        frisbeeFlinger = new FrisbeeFlinger(4, 6);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    driveTrain = new DriveTrain(5, 10);
-    driveCommand = new DriveCommand(driveTrain, controller);
-    driveTrain.setDefaultCommand(driveCommand);
-    configureBindings();
-  }
+        // Configure the trigger bindings
+        driveTrain = new DriveTrain(5, 10);
+        driveTrain.setDefaultCommand(new DriveCommand(driveTrain, controller));
+        configureBindings();
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    
-  }
+    }
+
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be
+     * created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+     * an arbitrary
+     * predicate, or via the named factories in {@link
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+     * {@link
+     * CommandXboxController
+     * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or
+     * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * joysticks}.
+     */
+    private void configureBindings() {
+        controller.rightTrigger.whileTrue(new ShooterCommand(frisbeeFlinger));
+        controller.rightTrigger.debounce(2)
+                               .and(controller.leftTrigger)
+                               .whileTrue(new FeederCommand(frisbeeFlinger));
+
+    }
 }
